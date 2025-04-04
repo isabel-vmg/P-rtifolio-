97f3077e276eaa6cdd6957214473585063875d64
Isabel Vitória <Contato.Isabel.Vitoria.MG@gmail.com>
2025-04-04 15:48:59 -0300

    Finalizando 1 versão

---
+++ js/script.js
@@ -0,0 +1,3 @@
+document.getElementById("asteroid").onclick = function() { alert("Este projeto ainda está em desenvolvimento!"); };
+document.getElementById("rock").onclick = function() { alert("Este projeto ainda está em desenvolvimento!"); };
+document.getElementById("google").onclick = function() { alert("Este projeto ainda está em desenvolvimento!"); };
